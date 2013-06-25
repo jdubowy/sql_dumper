0b2fa51dd9349683dd46d6ccfbedf63d1d2792bc
Joel Dubowy <joel@urbanspoon.com>
2013-06-25 10:57:55 -0700

    Support date and time values

--- lib/sql_dumper.rb
+++ lib/sql_dumper.rb
@@ -5,6 +5,8 @@ module SqlDumper
     excluded_columns = options[:excluded_columns] || []
     column_names = self.class.columns.map{ |c| c.name.to_sym }
     set_str = column_names.reject{ |n| excluded_columns.include?(n) }.map { |k|
+      # TODO:  try to grab raw values from db (if persisted, or even if not if
+      # active record somehow supports giving raw db vaues)
       v = send(k)
       if v.is_a?(String)
         v = "'#{v}'"
@@ -14,6 +16,12 @@ module SqlDumper
         v = 1
       elsif v.is_a?(FalseClass)
         v = 0
+      elsif v.is_a?(Date)
+        v = v.strftime("'%Y-%m-%d'")
+      elsif v.respond_to?(:strftime)
+        # TODO: make sure it's UTC...
+        v = v.utc if v.respond_to?(:utc)
+        v = v.strftime("'%Y-%m-%d %H:%M:%S'")
       end
       "`#{k}`=#{v}"
     }.join(', ')
--- test/test_dump.rb
+++ test/test_dump.rb
@@ -1,10 +1,11 @@
 require "minitest/autorun"
 require "sql_dumper"
+require "date"
 require 'ostruct'
 
 class DummyModel
   include SqlDumper
-  attr_accessor :id, :c_int, :c_float, :c_str, :c_bool, :c_null
+  attr_accessor :id, :c_int, :c_float, :c_str, :c_bool, :c_null, :c_date, :c_time
 
   def self.columns
     [
@@ -13,7 +14,9 @@ class DummyModel
       OpenStruct.new(name: "c_float"),
       OpenStruct.new(name: "c_str"),
       OpenStruct.new(name: "c_bool"),
-      OpenStruct.new(name: "c_null")
+      OpenStruct.new(name: "c_null"),
+      OpenStruct.new(name: "c_date"),
+      OpenStruct.new(name: "c_time")
     ]
   end
 
@@ -28,6 +31,8 @@ class DummyModel
     @c_str = "SDF"
     @c_bool = true
     @c_null = nil
+    @c_date = Date.new(2013, 1, 2)
+    @c_time = Time.new(2014, 7, 23, 3, 43, 5, -3600)
   end
 end
 
@@ -38,15 +43,18 @@ describe :SqlDumper do
 
   describe "when asked to dump with no exclusions" do
     it "returns insert with all columns" do
-      expected = "INSERT INTO foo_bars SET `id`=1, `c_int`=123, `c_float`=234.234, `c_str`='SDF', `c_bool`=1, `c_null`=NULL;"
+      expected = "INSERT INTO foo_bars SET `id`=1, `c_int`=123, " +
+        "`c_float`=234.234, `c_str`='SDF', `c_bool`=1, `c_null`=NULL, " +
+        "`c_date`='2013-01-02', `c_time`='2014-07-23 04:43:05';"
       assert_equal expected, @object.to_sql
     end
   end
 
   describe "when asked to dump with exclusions" do
     it "returns insert lacking exclusions" do
-      expected = "INSERT INTO foo_bars SET `c_int`=123, `c_str`='SDF', `c_bool`=1, `c_null`=NULL;"
-      assert_equal expected, @object.to_sql(excluded_columns: [:id, :c_float])
+      expected = "INSERT INTO foo_bars SET `c_int`=123, `c_str`='SDF', " +
+      "`c_bool`=1, `c_null`=NULL, `c_date`='2013-01-02';"
+      assert_equal expected, @object.to_sql(excluded_columns: [:id, :c_float, :c_time])
     end
   end
 end
